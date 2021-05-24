feacbcbcf40d5e3cd11777fde0d6d3618ab3b21c
Pooja G <poam@amazon.com>
2021-05-24 08:02:24 +0530

    Added Method Definitions for BST

---
+++ BinarySearchTreeImpl.java
@@ -0,0 +1,19 @@
+class BinarySearchTreeImpl {
+    
+}
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+class BinarySearchTree{
+
+    
+
+}
